5cbdece9b18e28abe202d51c83479af93bd0b934
ant0296 <61244567+ant0296@users.noreply.github.com>
2020-03-02 11:48:43 -0700

    Add files via upload
    Java and class files for Exercises 1-5, 1-9, and 1-13 of Chapter 1

---
+++ Exercise1dash9.java
@@ -0,0 +1,7 @@
+/*Anthony Catalano, 3/2, Exercise 1-9: Calculating area and perimeter of a rectangle*/
+public class Exercise1dash9 {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println(2 * 4.5 + 2 * 7.9);
+	}
+}
