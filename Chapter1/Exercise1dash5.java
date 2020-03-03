8e4b1a09f45d5d300e3545549e27864e85f422fc
ant0296 <61244567+ant0296@users.noreply.github.com>
2020-03-03 10:27:45 -0700

    Add files via upload

---
+++ Chapter1/Exercise1dash5.java
@@ -0,0 +1,7 @@
+/*Anthony Catalano, 3/2, Exercise 1-5: Performing math with programming*/
+public class Exercise1dash9 {
+	public static void main(String[] args) {
+		System.out.println(4.5 * 7.9);
+		System.out.println(2 * 4.5 + 2 *7.9);
+	}
+}
