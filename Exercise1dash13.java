afbf337c4f492cbe722be78e22d5343f652bd808
ant0296 <61244567+ant0296@users.noreply.github.com>
2020-03-03 10:28:10 -0700

    Delete Exercise1dash13.java

--- Exercise1dash13.java
+++
@@ -1,8 +0,0 @@
-/*Anthony Catalano, 3/2, Exercise 1-13: Solving for X and Y using Cramer's Rule, and printing the quotient*/
-class Exercise1dash13 {
-	public static void main(String[] args) {
-			System.out.println(-271.705 / -103.55);
-			System.out.println(-73.39 / -103.55);
-		
-	}
-}
